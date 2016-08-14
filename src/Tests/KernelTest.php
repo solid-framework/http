16a420a583f8baa549a8f461cec959c6fa0f10db
Martin Pettersson <martin_pettersson@outlook.com>
2016-08-14 11:15:23 +0200

    Added empty kernel test

---
+++ src/Tests/KernelTest.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Copyright (c) 2016 Martin Pettersson
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Solid\Http\Tests;
+
+use PHPUnit_Framework_TestCase;
+use Solid\Http\Kernel;
+
+/**
+ * @package Solid\Http\Tests
+ * @author Martin Pettersson <martin@solid-framework.com>
+ * @since 0.1.0
+ */
+class KernelTest extends PHPUnit_Framework_TestCase
+{
+    // ...
+}

6168fa73cd02f5e2a522ef2236dce0321527bbc8
Martin Pettersson <martin_pettersson@outlook.com>
2016-08-18 15:36:10 +0200

    Added request and test

---
+++ src/Request.php
@@ -0,0 +1,420 @@
+<?php
+
+/**
+ * Copyright (c) 2016 Martin Pettersson
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Solid\Http;
+
+use InvalidArgumentException;
+use Solid\Kernel\Request as KernelRequest;
+use Solid\Kernel\RequestInterface as KernelRequestInterface;
+use Psr\Http\Message\UriInterface;
+use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\RequestInterface;
+
+/**
+ * @package Solid\Http
+ * @author Martin Pettersson <martin@solid-framework.com>
+ * @since 0.1.0
+ */
+class Request implements KernelRequestInterface, RequestInterface
+{
+    /**
+     * @api
+     * @since 0.1.0
+     * @var array
+     */
+    const SUPPORTED_METHODS = [
+        'HEAD',
+        'GET',
+        'POST',
+        'PUT',
+        'DELETE',
+        'TRACE',
+        'OPTIONS',
+        'CONNECT',
+        'PATCH'
+    ];
+
+    /**
+     * @internal
+     * @since 0.1.0
+     * @var string
+     */
+    protected $method;
+
+    /**
+     * @internal
+     * @since 0.1.0
+     * @var string
+     */
+    protected $protocolVersion;
+
+    /**
+     * @internal
+     * @since 0.1.0
+     * @var HeaderContainer
+     */
+    protected $headers;
+
+    /**
+     * @internal
+     * @since 0.1.0
+     * @var string
+     */
+    protected $target;
+
+    /**
+     * @internal
+     * @since 0.1.0
+     * @var UriInterface
+     */
+    protected $uri;
+
+    /**
+     * @internal
+     * @since 0.1.0
+     * @var StreamInterface
+     */
+    protected $body;
+
+    /**
+     * @internal
+     * @since 0.1.0
+     * @param string|null          $method  The request method to use.
+     * @param Uri|null             $uri     The request uri to use.
+     * @param HeaderContainer|null $headers The request headers to use.
+     * @param StreamInterface|null $body    The request body to use.
+     */
+    private function __construct(
+        string $method = null,
+        Uri $uri = null,
+        HeaderContainer $headers = null,
+        StreamInterface $body = null
+    ) {
+        $this->protocolVersion = '1.1';
+        $this->method = $method;
+        $this->uri = $uri ?? new Uri;
+        $this->headers = $headers ?? new HeaderContainer;
+        $this->body = $body ?? new StringStream;
+
+        if (strlen($this->uri->getHost()) > 0 && !$this->headers->has('Host')) {
+            $this->headers->set('Host', $this->uri->getHost());
+        }
+    }
+
+    /**
+     * @internal
+     * @since 0.1.0
+     */
+    private function __clone()
+    {
+        $this->headers = clone $this->headers;
+        $this->uri = clone $this->uri;
+        $this->body = clone $this->body;
+    }
+
+    /**
+     * @api
+     * @since 0.1.0
+     * @param KernelRequest $request A kernel request to generate the implementation specific request from.
+     * @return KernelRequestInterface
+     */
+    public static function fromKernelRequest(KernelRequest $request): KernelRequestInterface
+    {
+        $serverParameters = self::normalizeServerParameters($request->getServerParameters());
+
+        $userInfo = $serverParameters['username'];
+        $host = $serverParameters['host'];
+
+        if (!is_null($serverParameters['port'])) {
+            $host .= ':' . $serverParameters['port'];
+        }
+
+        if (!empty($userInfo)) {
+            if (!empty($serverParameters['password'])) {
+                $userInfo .= ':' . $serverParameters['password'];
+            }
+
+            $userInfo .= '@';
+        }
+
+        return new self(
+            $serverParameters['method'],
+            new Uri(
+                $serverParameters['scheme'] . '://' .
+                $userInfo .
+                $host .
+                $serverParameters['path']
+            ),
+            new HeaderContainer($request->getHeaderParameters()),
+            new StringStream(file_get_contents('php://input'))
+        );
+    }
+
+    /**
+     * @internal
+     * @since 0.1.0
+     * @param array $serverParameters The server parameters to normalize.
+     * @return array
+     */
+    private static function normalizeServerParameters(array $serverParameters): array
+    {
+        $method = $serverParameters['REQUEST_METHOD'] ?? '';
+        $port = $serverParameters['SERVER_PORT'] ?? null;
+
+        $scheme = 'http';
+
+        if (array_key_exists('REQUEST_SCHEME', $serverParameters)) {
+            $scheme = $serverParameters['REQUEST_SCHEME'];
+        } elseif ((array_key_exists('HTTPS', $serverParameters) && $serverParameters['HTTPS'] === 'on') || $port === 443) {
+            $scheme = 'https';
+        }
+
+        $username = $serverParameters['PHP_AUTH_USER'] ?? '';
+        $password = $serverParameters['PHP_AUTH_PW'] ?? '';
+        $host = $serverParameters['HTTP_HOST'] ?? '';
+        $path = $serverParameters['REQUEST_URI'] ?? '';
+
+        return compact('method', 'scheme', 'host', 'port', 'path', 'username', 'password');
+    }
+
+    /**
+     * @api
+     * @since 0.1.0
+     * @return string
+     */
+    public function getProtocolVersion(): string
+    {
+        return $this->protocolVersion;
+    }
+
+    /**
+     * @api
+     * @since 0.1.0
+     * @param string $version HTTP protocol version
+     * @return Request
+     */
+    public function withProtocolVersion($version): self
+    {
+        $newRequest = clone $this;
+        $newRequest->protocolVersion = $version;
+
+        return $newRequest;
+    }
+
+    /**
+     * @api
+     * @since 0.1.0
+     * @return array
+     */
+    public function getHeaders(): array
+    {
+        return $this->headers->get();
+    }
+
+    /**
+     * @api
+     * @since 0.1.0
+     * @param string $name The header to check for.
+     * @return bool
+     */
+    public function hasHeader($name): bool
+    {
+        return $this->headers->has($name);
+    }
+
+    /**
+     * @api
+     * @since 0.1.0
+     * @param string $name The header to get.
+     * @return array
+     */
+    public function getHeader($name): array
+    {
+        return $this->headers->get($name);
+    }
+
+    /**
+     * @api
+     * @since 0.1.0
+     * @param string $name The header to get.
+     * @return string
+     */
+    public function getHeaderLine($name): string
+    {
+        return implode(',', $this->headers->get($name));
+    }
+
+    /**
+     * @api
+     * @since 0.1.0
+     * @param string          $name  Case-insensitive header field name.
+     * @param string|string[] $value Header value(s).
+     * @return Request
+     * @throws InvalidArgumentException
+     */
+    public function withHeader($name, $value): self
+    {
+        $newRequest = clone $this;
+        $newRequest->headers->set($name, $value);
+
+        return $newRequest;
+    }
+
+    /**
+     * @api
+     * @since 0.1.0
+     * @param string          $name  Case-insensitive header field name to add.
+     * @param string|string[] $value Header value(s).
+     * @return Request
+     * @throws InvalidArgumentException
+     */
+    public function withAddedHeader($name, $value): self
+    {
+        $newRequest = clone $this;
+        $newRequest->headers->add($name, $value);
+
+        return $newRequest;
+    }
+
+    /**
+     * @api
+     * @since 0.1.0
+     * @param string $name The header to remove.
+     * @return self
+     */
+    public function withoutHeader($name)
+    {
+        $newRequest = clone $this;
+        $newRequest->headers->remove($name);
+
+        return $newRequest;
+    }
+
+    /**
+     * @api
+     * @since 0.1.0
+     * @return StreamInterface
+     */
+    public function getBody(): StreamInterface
+    {
+        return $this->body;
+    }
+
+    /**
+     * @api
+     * @since 0.1.0
+     * @param StreamInterface $body The new body to use.
+     * @return self
+     * @throws InvalidArgumentException
+     */
+    public function withBody(StreamInterface $body)
+    {
+        $newRequest = clone $this;
+        $newRequest->body = $body;
+
+        return $newRequest;
+    }
+
+    /**
+     * @api
+     * @since 0.1.0
+     * @return string
+     */
+    public function getRequestTarget(): string
+    {
+        if (!is_null($this->target)) {
+            return $this->target;
+        }
+
+        $path = $this->uri->getPath();
+        $query = $this->uri->getQuery();
+
+        if (strlen($query) > 0) {
+            $path .= '?' . $query;
+        }
+
+        return strlen($path) > 0 ? $path : '/';
+    }
+
+    /**
+     * @api
+     * @since 0.1.0
+     * @param mixed $requestTarget The new request target to use.
+     * @return Request
+     */
+    public function withRequestTarget($requestTarget): self
+    {
+        $newRequest = clone $this;
+        $newRequest->target = $requestTarget;
+
+        return $newRequest;
+    }
+
+    /**
+     * @api
+     * @since 0.1.0
+     * @return string
+     */
+    public function getMethod(): string
+    {
+        return $this->method;
+    }
+
+    /**
+     * @api
+     * @since 0.1.0
+     * @param string $method The new request method to use.
+     * @return Request
+     * @throws InvalidArgumentException
+     */
+    public function withMethod($method): self
+    {
+        if (!in_array(strtolower($method), array_map('strtolower', self::SUPPORTED_METHODS))) {
+            throw new InvalidArgumentException('Unsupported request method: ' . $method);
+        }
+
+        $newRequest = clone $this;
+        $newRequest->method = $method;
+
+        return $newRequest;
+    }
+
+    /**
+     * @api
+     * @since 0.1.0
+     * @return UriInterface
+     */
+    public function getUri(): UriInterface
+    {
+        return $this->uri;
+    }
+
+    /**
+     * @api
+     * @since 0.1.0
+     * @param UriInterface $uri          The new request uri to use.
+     * @param bool         $preserveHost Whether to preserve the original host.
+     * @return Request
+     */
+    public function withUri(UriInterface $uri, $preserveHost = false): self
+    {
+        $newRequest = clone $this;
+        $newRequest->uri = $uri;
+
+        $host = $newRequest->getUri()->getHost();
+
+        if (strlen($host) > 0) {
+            if (!$preserveHost || !$newRequest->headers->has('Host')) {
+                $newRequest->headers->set('Host', $host);
+            }
+        }
+
+        return $newRequest;
+    }
+}
---
+++ src/Tests/RequestTest.php
@@ -0,0 +1,481 @@
+<?php
+
+/**
+ * Copyright (c) 2016 Martin Pettersson
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Solid\Http\Tests;
+
+use Solid\Kernel\Request as KernelRequest;
+use Solid\Http\Uri;
+use Solid\Http\Request;
+use Solid\Http\StringStream;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @package Solid\Http\Tests
+ * @author Martin Pettersson <martin@solid-framework.com>
+ * @since 0.1.0
+ */
+class RequestTest extends TestCase
+{
+    /**
+     * @internal
+     * @since 0.1.0
+     * @var Request
+     */
+    protected $getRequest;
+
+    /**
+     * @internal
+     * @since 0.1.0
+     * @var Request
+     */
+    protected $postRequest;
+
+    /**
+     * @internal
+     * @since 0.1.0
+     * @var Request
+     */
+    protected $emptyRequest;
+
+    /**
+     * @api
+     * @before
+     * @since 0.1.0
+     * @return void
+     */
+    public function setup()
+    {
+        $this->getRequest = Request::fromKernelRequest(new Fixtures\GetKernelRequest);
+        $this->postRequest = Request::fromKernelRequest(new Fixtures\PostKernelRequest);
+        $this->putRequest = Request::fromKernelRequest(new Fixtures\PutKernelRequest);
+        $this->emptyRequest = Request::fromKernelRequest(new Fixtures\EmptyKernelRequest);
+    }
+
+    /**
+     * @api
+     * @test
+     * @since 0.1.0
+     * @return void
+     */
+    public function testImplementationRequirements()
+    {
+        $this->assertInstanceOf(
+            'Solid\Kernel\RequestInterface',
+            $this->getRequest,
+            'Should implement kernel request interface'
+        );
+        $this->assertInstanceOf(
+            'Psr\Http\Message\RequestInterface',
+            $this->getRequest,
+            'Should implement PSR-7 request interface'
+        );
+    }
+
+    /**
+     * @api
+     * @test
+     * @since 0.1.0
+     * @return void
+     */
+    public function testGetProtocolVersion()
+    {
+        $this->assertSame('1.1', $this->getRequest->getProtocolVersion(), 'Should return correct protocol version');
+    }
+
+    /**
+     * @api
+     * @test
+     * @since 0.1.0
+     * @return void
+     */
+    public function testWithProtocolVersion()
+    {
+        $newProtocolVersion = $this->getRequest->withProtocolVersion('2.0');
+        $this->assertInstanceOf('Solid\Http\Request', $newProtocolVersion, 'Should return new instance');
+        $this->assertNotSame($this->getRequest, $newProtocolVersion, 'Should return new instance');
+        $this->assertSame('1.1', $this->getRequest->getProtocolVersion(), 'Should not mutate the original request');
+        $this->assertSame('2.0', $newProtocolVersion->getProtocolVersion(), 'Should return correct protocol version');
+    }
+
+    /**
+     * @api
+     * @test
+     * @since 0.1.0
+     * @return void
+     */
+    public function testGetHeaders()
+    {
+        $this->assertSame([], $this->emptyRequest->getHeaders(), 'Should return the correct headers');
+        $this->assertSame(
+            [
+                'Content-Length' => [0],
+                'Accept' => ['application/json'],
+                'Host' => ['example.com']
+            ],
+            $this->getRequest->getHeaders(),
+            'Should return the correct headers'
+        );
+    }
+
+    /**
+     * @api
+     * @test
+     * @since 0.1.0
+     * @return void
+     */
+    public function testGetHeader()
+    {
+        $this->assertSame(
+            ['example.com'],
+            $this->getRequest->getHeader('Host'),
+            'Should be able to retrieve a single header field'
+        );
+        $this->assertSame(
+            ['example.com'],
+            $this->getRequest->getHeader('hoSt'),
+            'Should be able to retrieve a single header field'
+        );
+        $this->assertSame(
+            [],
+            $this->getRequest->getHeader('Non-Existing-Header'),
+            'Should return an empty array if the header field does not exist'
+        );
+    }
+
+    /**
+     * @api
+     * @test
+     * @since 0.1.0
+     * @return void
+     */
+    public function testGetHeaderLine()
+    {
+        $this->assertSame(
+            'example.com',
+            $this->getRequest->getHeaderLine('Host'),
+            'Should return the correct headerline'
+        );
+        $this->assertSame(
+            'example.com',
+            $this->getRequest->getHeaderLine('hoSt'),
+            'Should return the correct headerline'
+        );
+
+        $multipleHeaders = $this->getRequest->withAddedHeader('Host', 'new-host.com');
+        $this->assertSame(
+            'example.com,new-host.com',
+            $multipleHeaders->getHeaderLine('Host'),
+            'Should return the correct headerline'
+        );
+
+        $this->assertSame(
+            '',
+            $this->getRequest->getHeaderLine('Non-Existing-Header'),
+            'Should return an empty string if the header field does not exist'
+        );
+    }
+
+    /**
+     * @api
+     * @test
+     * @since 0.1.0
+     * @return void
+     */
+    public function testHasHeader()
+    {
+        $this->assertTrue($this->getRequest->hasHeader('Host'), 'Should be able to determine if a header is set');
+        $this->assertTrue($this->getRequest->hasHeader('hoSt'), 'Should be able to determine if a header is set');
+        $this->assertFalse($this->emptyRequest->hasHeader('host'), 'Should be able to determine if a header is set');
+    }
+
+    /**
+     * @api
+     * @test
+     * @since 0.1.0
+     * @return void
+     */
+    public function testWithHeader()
+    {
+        $newHost = $this->getRequest->withHeader('host', 'new-host.com');
+        $this->assertInstanceOf('Solid\Http\Request', $newHost, 'Should return new instance');
+        $this->assertNotSame($this->getRequest, $newHost, 'Should return new instance');
+        $this->assertSame(
+            ['example.com'],
+            $this->getRequest->getHeader('host'),
+            'Should not mutate the original request'
+        );
+        $this->assertSame(['new-host.com'], $newHost->getHeader('hOsT'), 'Should be able to set new header value');
+    }
+
+    /**
+     * @api
+     * @test
+     * @since 0.1.0
+     * @return void
+     */
+    public function testWithAddedHeader()
+    {
+        $newHost = $this->getRequest->withAddedHeader('host', 'new-host.com');
+        $this->assertInstanceOf('Solid\Http\Request', $newHost, 'Should return new instance');
+        $this->assertNotSame($this->getRequest, $newHost, 'Should return new instance');
+        $this->assertSame(
+            ['example.com'],
+            $this->getRequest->getHeader('host'),
+            'Should not mutate the original request'
+        );
+        $this->assertSame(
+            ['example.com', 'new-host.com'],
+            $newHost->getHeader('hOsT'),
+            'Should be able to set new header value'
+        );
+    }
+
+    /**
+     * @api
+     * @test
+     * @since 0.1.0
+     * @return void
+     */
+    public function testWithoutHeader()
+    {
+        $noHost = $this->getRequest->withoutHeader('hoSt');
+        $this->assertInstanceOf('Solid\Http\Request', $noHost, 'Should return new instance');
+        $this->assertNotSame($this->getRequest, $noHost, 'Should return new instance');
+        $this->assertSame(
+            ['example.com'],
+            $this->getRequest->getHeader('host'),
+            'Should not mutate the original request'
+        );
+        $this->assertSame(
+            [],
+            $noHost->getHeader('Host'),
+            'Should be able to remove headers'
+        );
+    }
+
+    /**
+     * @api
+     * @test
+     * @since 0.1.0
+     * @return void
+     */
+    public function testGetBody()
+    {
+        $getBody = $this->getRequest->getBody();
+        $putBody = $this->putRequest->getBody();
+        $postBody = $this->postRequest->getBody();
+
+        $this->assertInstanceOf(
+            'Psr\Http\Message\StreamInterface',
+            $getBody,
+            'Should return a PSR-7 string stream object'
+        );
+
+        $this->assertSame('', (string) $getBody, 'Should return the correct body object');
+        $this->assertSame('', (string) $putBody, 'Should return the correct body object');
+        $this->assertSame('', (string) $postBody, 'Should return the correct body object');
+    }
+
+    /**
+     * @api
+     * @test
+     * @since 0.1.0
+     * @return void
+     */
+    public function testWithBody()
+    {
+        $newBody = $this->postRequest->withBody(new StringStream('This is the body'));
+
+        $this->assertInstanceOf('Solid\Http\Request', $newBody, 'Should return new instance');
+        $this->assertNotSame($this->getRequest, $newBody, 'Should return new instance');
+        $this->assertSame(
+            '',
+            (string) $this->getRequest->getBody(),
+            'Should not mutate the original request'
+        );
+        $this->assertSame(
+            'This is the body',
+            (string) $newBody->getBody(),
+            'Should be able to set new body'
+        );
+    }
+
+    /**
+     * @api
+     * @test
+     * @since 0.1.0
+     * @return void
+     */
+    public function testGetRequestTarget()
+    {
+        $this->assertSame(
+            '/',
+            $this->emptyRequest->getRequestTarget(),
+            'Should return correct request target'
+        );
+        $this->assertSame(
+            '/example/path?parameter1=value1&parameter2=value2',
+            $this->getRequest->getRequestTarget(),
+            'Should return correct request target'
+        );
+        $this->assertSame(
+            '/',
+            $this->postRequest->getRequestTarget(),
+            'Should return correct request target'
+        );
+        $this->assertSame(
+            '/path',
+            $this->putRequest->getRequestTarget(),
+            'Should return correct request target'
+        );
+    }
+
+    /**
+     * @api
+     * @test
+     * @since 0.1.0
+     * @return void
+     */
+    public function testWithRequestTarget()
+    {
+        $newRequest = $this->getRequest->withRequestTarget('/new/path');
+
+        $this->assertInstanceOf('Solid\Http\Request', $newRequest, 'Should return new instance');
+        $this->assertNotSame($this->getRequest, $newRequest, 'Should return new instance');
+        $this->assertSame(
+            '/example/path?parameter1=value1&parameter2=value2',
+            $this->getRequest->getRequestTarget(),
+            'Should not mutate the original request'
+        );
+        $this->assertSame('/new/path', $newRequest->getRequestTarget(), 'Should be able to set request target');
+    }
+
+    /**
+     * @api
+     * @test
+     * @since 0.1.0
+     * @return void
+     */
+    public function testGetMethod()
+    {
+        $this->assertSame('GET', $this->getRequest->getMethod(), 'Should return the correct request method');
+        $this->assertSame('post', $this->postRequest->getMethod(), 'Should return the correct request method');
+        $this->assertSame('Put', $this->putRequest->getMethod(), 'Should return the correct request method');
+    }
+
+    /**
+     * @api
+     * @test
+     * @since 0.1.0
+     * @return void
+     */
+    public function testWithMethod()
+    {
+        $newRequest = $this->getRequest->withMethod('DelEtE');
+
+        $this->assertInstanceOf('Solid\Http\Request', $newRequest, 'Should return new instance');
+        $this->assertNotSame($this->getRequest, $newRequest, 'Should return new instance');
+        $this->assertSame(
+            'GET',
+            $this->getRequest->getMethod(),
+            'Should not mutate the original request'
+        );
+        $this->assertSame(
+            'DelEtE',
+            $newRequest->getMethod(),
+            'Should be able to set request method (case insensitive)'
+        );
+    }
+
+    /**
+     * @api
+     * @test
+     * @since 0.1.0
+     * @return void
+     * @expectedException InvalidArgumentException
+     */
+    public function testWithInvalidMethod()
+    {
+        $newRequestt = $this->getRequest->withMethod('UNSUPPORTED_METHOD');
+    }
+
+    /**
+     * @api
+     * @test
+     * @since 0.1.0
+     * @return void
+     */
+    public function testGetUri()
+    {
+        $this->assertInstanceOf(
+            'Psr\Http\Message\UriInterface',
+            $this->getRequest->getUri(),
+            'Should return a Uri instance'
+        );
+        $this->assertSame(
+            'http://username:password@example.com/example/path?parameter1=value1&parameter2=value2',
+            (string) $this->getRequest->getUri(),
+            'Should return the correct uri instance'
+        );
+    }
+
+    /**
+     * @api
+     * @test
+     * @since 0.1.0
+     * @return void
+     */
+    public function testWithUri()
+    {
+        $withHost = $this->getRequest->withUri(new Uri('new-example.com/new/path'));
+        $this->assertInstanceOf('Solid\Http\Request', $withHost, 'Should return new instance');
+        $this->assertNotSame($this->getRequest, $withHost, 'Should return new instance');
+        $this->assertSame(
+            'example.com',
+            $this->getRequest->getUri()->getHost(),
+            'Should not mutate the original request'
+        );
+        $this->assertSame('new-example.com', $withHost->getUri()->getHost(), 'Should be able to set url');
+        $this->assertSame('new-example.com', $withHost->getHeader('Host')[0], 'Should update host header');
+
+        $emptyUri = $this->getRequest->withUri(new Uri);
+        $this->assertSame(
+            'example.com',
+            $emptyUri->getHeader('Host')[0],
+            'Should carry over host header if no host is provided in the new uri'
+        );
+
+        $newHost = $this->getRequest->withUri(new Uri('another-example.com'));
+        $this->assertSame(
+            'another-example.com',
+            $newHost->getHeader('Host')[0],
+            'Should update the host header if new host is provided'
+        );
+
+        $newPreservedHost = $this->getRequest->withUri(new Uri('another-example.com'), true);
+        $this->assertSame(
+            'example.com',
+            $newPreservedHost->getHeader('Host')[0],
+            'Should be able to preserve host if desired'
+        );
+
+        $newEmptyHost = $this->emptyRequest->withUri(new Uri, true);
+        $this->assertEmpty(
+            $newEmptyHost->getHeader('Host'),
+            'Should not update the host header if no new host is provided'
+        );
+
+        $newPreservedEmptyHost = $this->emptyRequest->withUri(new Uri('example.com'), true);
+        $this->assertSame(
+            'example.com',
+            $newPreservedEmptyHost->getHeader('Host')[0],
+            'Should always update host header if previous host was empty and a new is provided'
+        );
+    }
+}

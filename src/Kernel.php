97b47db2d1103c9d7439cf17daf6b3da3e40e1e6
Martin Pettersson <martin_pettersson@outlook.com>
2016-09-01 19:13:56 +0200

    Added kernel and test

---
+++ src/Kernel.php
@@ -0,0 +1,139 @@
+<?php
+
+/**
+ * Copyright (c) 2016 Martin Pettersson
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Solid\Http;
+
+use Exception;
+use Solid\Container\Container;
+use Psr\Http\Message\RequestInterface;
+use Solid\Kernel\KernelInterface;
+use Solid\Kernel\RequestInterface as KernelRequestInterface;
+use Solid\Kernel\ResponseInterface as KernelResponseInterface;
+use Solid\Kernel\ResourceNotFoundException;
+use Solid\Kernel\UnsupportedRequestTypeException;
+use Solid\Kernel\UnsupportedResponseTypeException;
+
+/**
+ * @package Solid\Http
+ * @author Martin Pettersson <martin@solid-framework.com>
+ * @since 0.1.0
+ */
+class Kernel implements KernelInterface
+{
+    /**
+     * An instance of the Http router
+     *
+     * @internal
+     * @since 0.1.0
+     * @var Router
+     */
+    protected $router;
+
+    /**
+     * The application DI container
+     *
+     * @internal
+     * @since 0.1.0
+     * @var Container
+     */
+    protected $container;
+
+    /**
+     * @api
+     * @since 0.1.0
+     * @param Router    $router    An instance of the Http router.
+     * @param Container $container The application DI container.
+     */
+    public function __construct(Router $router, Container $container)
+    {
+        $this->router = $router;
+        $this->container = $container;
+    }
+
+    /**
+     * @api
+     * @since 0.1.0
+     * @param KernelRequestInterface $request The request to handle.
+     * @return KernelResponseInterface
+     * @throws UnsupportedRequestTypeException
+     */
+    public function handleRequest(KernelRequestInterface $request): KernelResponseInterface
+    {
+        // only handle PSR-7 compliant HTTP requests
+        if (!$request instanceof RequestInterface) {
+            throw new UnsupportedRequestTypeException;
+        }
+
+        // bind kernel specific request
+        $this->container->instance('request', $request);
+
+        $response = $this->container->resolve('Solid\Http\Response');
+
+        try {
+            $resourceResponse = $this->router->routeRequest($request);
+
+            return $response
+                ->withStatus(200)
+                ->withBody(new StringStream($resourceResponse));
+        } catch (ResourceNotFoundException $exception) {
+            $statusCode = 404;
+            $message = $exception->getMessage();
+
+            if (strlen($message) === 0) {
+                $message = Response::STATUS_CODES[$statusCode];
+            }
+
+            return $response
+                ->withStatus($statusCode)
+                ->withBody(new StringStream($message));
+        } catch (Exception $exception) {
+            $statusCode = 500;
+            $message = $exception->getMessage();
+
+            if (strlen($message) === 0) {
+                $message = Response::STATUS_CODES[$statusCode];
+            }
+
+            return $response
+                ->withStatus(500)
+                ->withBody(new StringStream($message));
+        }
+    }
+
+    /**
+     * @api
+     * @since 0.1.0
+     * @param KernelResponseInterface $response The response to dispatch.
+     * @return void
+     * @throws UnsupportedResponseTypeException
+     */
+    public function dispatchResponse(KernelResponseInterface $response)
+    {
+        if (!$response instanceof Response) {
+            throw new UnsupportedResponseTypeException;
+        }
+
+        // set initial response header
+        @header(
+            "HTTP/{$response->getProtocolVersion()} {$response->getStatusCode()} {$response->getReasonPhrase()}",
+            true,
+            $response->getStatusCode()
+        );
+
+        @header('Content-Length: ' . $response->getBody()->getSize());
+
+        // set registered response headers
+        foreach ($response->getHeaders() as $header => $value) {
+            @header($header . ': ' . implode(',', $value));
+        }
+
+        // send the response
+        echo $response->getBody()->getContents();
+    }
+}
---
+++ src/Tests/KernelTest.php
@@ -0,0 +1,242 @@
+<?php
+
+/**
+ * Copyright (c) 2016 Martin Pettersson
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Solid\Http\Tests;
+
+use Solid\Http\Kernel;
+use Solid\Http\Request;
+use Solid\Http\Response;
+use Solid\Http\StringStream;
+use Solid\Kernel\Request as KernelRequest;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @package Solid\Http\Tests
+ * @author Martin Pettersson <martin@solid-framework.com>
+ * @since 0.1.0
+ * @coversDefaultClass Solid\Http\Kernel
+ */
+class KernelTest extends TestCase
+{
+    /**
+     * @internal
+     * @since 0.1.0
+     * @var \Solid\Container\Container
+     */
+    protected $containerMock;
+
+    /**
+     * @internal
+     * @since 0.1.0
+     * @var \Solid\Http\Router
+     */
+    protected $routerMock;
+
+    /**
+     * @api
+     * @before
+     * @since 0.1.0
+     * @return void
+     */
+    public function setup()
+    {
+        $this->containerMock = $this->createMock('Solid\Container\Container');
+        $this->routerMock = $this->createMock('Solid\Http\Router');
+        $this->emptyRequest = Request::fromKernelRequest(new KernelRequest([], [], [], [], [], [], [], [], [], []));
+        $this->emptyResponse = new Response;
+    }
+
+    /**
+     * @api
+     * @test
+     * @covers ::__construct
+     * @since 0.1.0
+     * @return void
+     */
+    public function testImplementationRequirements()
+    {
+        $kernel = new Kernel($this->routerMock, $this->containerMock);
+
+        $this->assertInstanceOf(
+            'Solid\Kernel\KernelInterface',
+            $kernel,
+            'Should implement the kernel interface'
+        );
+    }
+
+    /**
+     * @api
+     * @test
+     * @covers ::handleRequest
+     * @expectedException Solid\Kernel\UnsupportedRequestTypeException
+     * @since 0.1.0
+     * @return void
+     */
+    public function testInvalidRequestType()
+    {
+        $kernel = new Kernel($this->routerMock, $this->containerMock);
+        $kernel->handleRequest($this->createMock('Solid\Kernel\RequestInterface'));
+    }
+
+    /**
+     * @api
+     * @test
+     * @covers ::handleRequest
+     * @since 0.1.0
+     * @return void
+     */
+    public function testRequestBinding()
+    {
+        $this->containerMock->method('resolve')->will($this->returnValue(new Response));
+        $this->containerMock->expects($this->once())
+            ->method('instance')
+            ->with('request', $this->emptyRequest);
+
+        $kernel = new Kernel($this->routerMock, $this->containerMock);
+        $response = $kernel->handleRequest($this->emptyRequest);
+    }
+
+    /**
+     * @api
+     * @test
+     * @covers ::handleRequest
+     * @since 0.1.0
+     * @return void
+     */
+    public function test200Response()
+    {
+        $this->containerMock->method('resolve')->will($this->returnValue(new Response));
+        $this->routerMock->method('routeRequest')->will($this->returnCallback(function () {
+            $testController = new \Solid\App\Controllers\TestController;
+
+            return $testController->allIndex();
+        }));
+        $request = $this->emptyRequest->withRequestTarget('/test');
+        $kernel = new Kernel($this->routerMock, $this->containerMock);
+        $response = $kernel->handleRequest($request);
+
+        $this->assertInstanceOf(
+            'Psr\Http\Message\ResponseInterface',
+            $response,
+            'Should return a PSR-7 compliant HTTP response object'
+        );
+        $this->assertSame(200, $response->getStatusCode(), 'Should return correct status code');
+        $this->assertSame(
+            'TestController::allIndex',
+            (string) $response->getBody(),
+            'Should return a response with the correct body'
+        );
+    }
+
+    /**
+     * @api
+     * @test
+     * @covers ::handleRequest
+     * @since 0.1.0
+     * @return void
+     */
+    public function test404Response()
+    {
+        $this->containerMock->method('resolve')->will($this->returnValue(new Response));
+        $this->routerMock->method('routeRequest')->will(
+            $this->throwException(new \Solid\Kernel\ControllerNotFoundException)
+        );
+        $request = $this->emptyRequest->withRequestTarget('/no/path');
+        $kernel = new Kernel($this->routerMock, $this->containerMock);
+        $response = $kernel->handleRequest($request);
+
+        $this->assertSame(404, $response->getStatusCode(), 'Should return correct status code');
+        $this->assertSame(
+            'Not Found',
+            (string) $response->getBody(),
+            'Should return a response with the correct body'
+        );
+    }
+
+    /**
+     * @api
+     * @test
+     * @covers ::handleRequest
+     * @since 0.1.0
+     * @return void
+     */
+    public function test500Response()
+    {
+        $this->containerMock->method('resolve')->will($this->returnValue(new Response));
+        $this->routerMock->method('routeRequest')->will(
+            $this->throwException(new \Exception)
+        );
+        $request = $this->emptyRequest->withRequestTarget('/no/path');
+        $kernel = new Kernel($this->routerMock, $this->containerMock);
+        $response = $kernel->handleRequest($request);
+
+        $this->assertSame(500, $response->getStatusCode(), 'Should return correct status code');
+        $this->assertSame(
+            'Internal Server Error',
+            (string) $response->getBody(),
+            'Should return a response with the correct body'
+        );
+    }
+
+    /**
+     * @api
+     * @test
+     * @covers ::handleRequest
+     * @since 0.1.0
+     * @return void
+     */
+    public function testExceptionMessage()
+    {
+        $this->containerMock->method('resolve')->will($this->returnValue(new Response));
+        $this->routerMock->method('routeRequest')->will(
+            $this->throwException(new \Exception('Exception message'))
+        );
+        $request = $this->emptyRequest->withRequestTarget('/no/path');
+        $kernel = new Kernel($this->routerMock, $this->containerMock);
+        $response = $kernel->handleRequest($request);
+
+        $this->assertSame(
+            'Exception message',
+            (string) $response->getBody(),
+            'Should return a response with the correct body'
+        );
+    }
+
+    /**
+     * @api
+     * @test
+     * @covers ::dispatchResponse
+     * @expectedException Solid\Kernel\UnsupportedResponseTypeException
+     * @since 0.1.0
+     * @return void
+     */
+    public function testInvalidResponseType()
+    {
+        $kernel = new Kernel($this->routerMock, $this->containerMock);
+        $kernel->dispatchResponse($this->createMock('Solid\Kernel\ResponseInterface'));
+    }
+
+    /**
+     * @api
+     * @test
+     * @covers ::dispatchResponse
+     * @since 0.1.0
+     * @return void
+     */
+    public function testResponseBody()
+    {
+        $kernel = new Kernel($this->routerMock, $this->containerMock);
+
+        ob_start();
+        $kernel->dispatchResponse($this->emptyResponse->withBody(new StringStream('response body')));
+        $output = ob_get_clean();
+
+        $this->assertSame('response body', $output, 'Should respond with the correct body');
+    }
+}
